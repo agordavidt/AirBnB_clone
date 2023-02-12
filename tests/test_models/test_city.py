cc2da68f8ed3ac719af5065a2062553d4cca283d
babatunde matthew <babs20001@gmail.com>
2023-02-12 22:16:37 +0000

    authors for AirBnB PROJECT

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,86 +1,53 @@
 #!/usr/bin/python3
+"""Unittest module for the City Class."""
+
 import unittest
-import pep8
-import json
-import os
 from datetime import datetime
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
+import time
 from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+import re
+import json
 from models.engine.file_storage import FileStorage
-
-
-class TestCityDocs(unittest.TestCase):
-    """ check for documentation """
-    def test_class_doc(self):
-        """ check for class documentation """
-        self.assertTrue(len(City.__doc__) > 0)
-
-
-class TestCityPep8(unittest.TestCase):
-    """ check for pep8 validation """
-    def test_pep8(self):
-        """ test base and test_base for pep8 conformance """
-        style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/city.py'
-        file2 = 'tests/test_models/test_city.py'
-        result = style.check_files([file1, file2])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warning).")
+import os
+from models import storage
+from models.base_model import BaseModel
 
 
 class TestCity(unittest.TestCase):
-    """ tests for class City """
-    @classmethod
-    def setUpClass(cls):
-        """ set up instances for all tests """
-        cls.city = City()
 
-    def test_subclass(self):
-        """ test that city is a subclass of basemodel """
-        self.assertIsInstance(self.city, BaseModel)
-        self.assertTrue(hasattr(self.city, "id"))
-        self.assertTrue(hasattr(self.city, "created_at"))
-        self.assertTrue(hasattr(self.city, "updated_at"))
+    """Test Cases for the City class."""
 
-    def test_id(self):
-        """ test id """
-        self.assertEqual(str, type(self.city.id))
-
-    def test_created_at(self):
-        """ test created_at """
-        self.assertEqual(datetime, type(self.city.created_at))
-
-    def test_updated_at(self):
-        """ test updated_at """
-        self.assertEqual(datetime, type(self.city.updated_at))
-
-    def test_name(self):
-        """ test name """
-        self.assertTrue(hasattr(self.city, "name"))
-        self.assertEqual(self.city.name, "")
-
-    def test_state_id(self):
-        """ test state id """
-        self.assertTrue(hasattr(self.city, "state_id"))
-        self.assertEqual(self.city.state_id, "")
-
-    def test_to_dict(self):
-        """ test to_dict method """
-        new_dict = self.city.to_dict()
-        self.assertEqual(type(new_dict), dict)
-        self.assertTrue('to_dict' in dir(self.city))
-
-    def test_str(self):
-        """ test ___str___ method """
-        correct = "[City] ({}) {}".format(self.city.id, self.city.__dict__)
-        self.assertEqual(correct, str(self.city))
-
-    @classmethod
-    def tearDownClass(cls):
-        """ remove test instances """
+    def setUp(self):
+        """Sets up test methods."""
         pass
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests instantiation of City class."""
+
+        b = City()
+        self.assertEqual(str(type(b)), "<class 'models.city.City'>")
+        self.assertIsInstance(b, City)
+        self.assertTrue(issubclass(type(b), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes of City class."""
+        attributes = storage.attributes()["City"]
+        o = City()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,86 +1,53 @@
 #!/usr/bin/python3
+"""Unittest module for the Review Class."""
+
 import unittest
-import pep8
-import json
-import os
 from datetime import datetime
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
+import time
 from models.review import Review
+import re
+import json
 from models.engine.file_storage import FileStorage
-
-
-class TestReviewDocs(unittest.TestCase):
-    """ check for documentation """
-    def test_class_doc(self):
-        """ check for class documentation """
-        self.assertTrue(len(Review.__doc__) > 0)
-
-
-class TestReviewPep8(unittest.TestCase):
-    """ check for pep8 validation """
-    def test_pep8(self):
-        """ test base and test_base for pep8 conformance """
-        style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/review.py'
-        file2 = 'tests/test_models/test_review.py'
-        result = style.check_files([file1, file2])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warning).")
+import os
+from models import storage
+from models.base_model import BaseModel
 
 
 class TestReview(unittest.TestCase):
-    """ tests for class Review """
-    @classmethod
-    def setUpClass(cls):
-        """ set up instances for all tests """
-        cls.review = Review()
 
-    def test_subclass(self):
-        """ test that review is a subclass of basemodel """
-        self.assertIsInstance(self.review, BaseModel)
-        self.assertTrue(hasattr(self.review, "id"))
-        self.assertTrue(hasattr(self.review, "created_at"))
-        self.assertTrue(hasattr(self.review, "updated_at"))
+    """Test Cases for the Review class."""
 
-    def test_id(self):
-        """ test id """
-        self.assertEqual(str, type(self.review.id))
-
-    def test_created_at(self):
-        """ test created_at """
-        self.assertEqual(datetime, type(self.review.created_at))
-
-    def test_updated_at(self):
-        """ test updated_at """
-        self.assertEqual(datetime, type(self.review.updated_at))
-
-    def test_place_id(self):
-        """ test place_id """
-        self.assertTrue(hasattr(self.review, "place_id"))
-        self.assertEqual(self.review.place_id, "")
-
-    def test_user_id(self):
-        """ test user_id """
-        self.assertTrue(hasattr(self.review, "user_id"))
-        self.assertEqual(self.review.user_id, "")
-
-    def test_text(self):
-        """ test text """
-        self.assertTrue(hasattr(self.review, "text"))
-        self.assertEqual(self.review.text, "")
-
-    def test_to_dict(self):
-        """ test to_dict method """
-        new_dict = self.review.to_dict()
-        self.assertEqual(type(new_dict), dict)
-        self.assertTrue('to_dict' in dir(self.review))
-
-    @classmethod
-    def tearDownClass(cls):
-        """ remove test instances """
+    def setUp(self):
+        """Sets up test methods."""
         pass
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests instantiation of Review class."""
+
+        b = Review()
+        self.assertEqual(str(type(b)), "<class 'models.review.Review'>")
+        self.assertIsInstance(b, Review)
+        self.assertTrue(issubclass(type(b), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes of Review class."""
+        attributes = storage.attributes()["Review"]
+        o = Review()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+
+if __name__ == "__main__":
+    unittest.main()
